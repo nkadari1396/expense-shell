6a50ecd14c58e82d56fd0b4a60868d663b65d335
Naveen Kadari <nkadari1396@gmail.com>
2026-03-09 23:18:52 +0800

    Installing Git

--- backend.sh
+++ backend.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-LOGS_FOLDER="var/log/expense"
+LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-TIMESTAMP.log"
-mkdir -p LOGS_FOLDER
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 
 USER_ID=$(id -u)
 
@@ -14,9 +14,9 @@ Y="\e[33m"
 N="\e[0m"
 
 CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
+    if [ $USER_ID -ne 0 ]
     then
-        echo -e "Please run this script with root privileges" &>>LOG_FOLDER
+        echo -e "Please run this script with root privileges" &>>$LOG_FILE
         exit 1
     fi
     
@@ -25,9 +25,9 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e $2 is... $R FAILED $N | tee -a &LOG_FOLDER
+        echo -e $2 is... $R FAILED $N | tee -a $LOG_FILE
     else
-        echo -e $2 is... $G FAILED $N | tee -a &LOG_FOLDER
+        echo -e $2 is... $G SUCCESS $N | tee -a $LOG_FILE
     fi
 
 }
@@ -36,18 +36,18 @@ echo "script started excuting at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf module enable nodejs -y &>>LOG_FILE
+dnf module enable nodejs -y &>>$LOG_FILE
 VALIDATE $? "Disable deault nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOG_FILE
 VALIADATE $? "Install nodejs"
 
-id expense &>>LOG_FOLDER
+id expense &>>$LOG_FILE
 
 if [ $? -ne 0 ]
 then
     echo -e "expense user not exsists.. $G Creating $N"
-    useradd expense &>>LOG_FILE
+    useradd expense &>>$LOG_FILE
     VALIDATE $? "Creating expense user"
 else
     echo -e "expense user alraedy exsist... $Y SKIPPING $N"
