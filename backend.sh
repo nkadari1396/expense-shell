c882fd7bde6963db86366118395717d5a78d1484
Naveen Kadari <nkadari1396@gmail.com>
2026-03-09 22:16:13 +0800

    Installing Git

---
+++ backend.sh
@@ -0,0 +1,85 @@
+#!\bin\bash
+
+LOGS_FOLDER="var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-TIMESTAMP
+mkdir -p LOGS_FOLDER
+
+USER_ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USEID -ne 0 ]
+    then
+        echo -e "Please run this script with root privileges" &>>LOG_FOLDER
+        exit 1
+    fi
+    
+    }
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e $2 is... $R FAILED $N | tee -a &LOG_FOLDER
+    else
+        echo -e $2 is... $G FAILED $N | tee -a &LOG_FOLDER
+    fi
+
+}
+
+echo "script started excuting at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf module enable nodejs -y &>>LOG_FILE
+VALIDATE $? "Disable deault nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIADATE $? "Install nodejs"
+
+id expense &>>LOG_FOLDER
+
+if [ $? -ne 0 ]
+then
+    echo -e "expense user not exsists.. $G Creating $N"
+    useradd expense &>>LOG_FILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "expense user alraedy exsist... $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "Creating /app folder"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOG_FILE
+VALIDATE $? "Downloading backend application code"
+
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip &>>LOG_FILE
+VALIDATE $? "Extracting backend application code"
+
+npm install &>>LOG_FILE
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+
+dnf install mysql -y &>>LOG_FILE
+VALIDATE $? "Installing MySQL client"
+
+mysql -h mysql.naveenkadari.com -uroot -pExpenseAPP@1 < /app/schema/backend.sql &>>LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl enable backend &>>LOG_FILE
+VALIDATE $? "Enabled backend"
+
+systemctl restart backend &>>LOG_FILE
+VALIDATE $? "restart backend"
+
+
---
+++ mysql.sh
@@ -0,0 +1,57 @@
+#!\bin\bash
+
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log
+mkdir -p $LOGS_FOLDER  #-p it checks whether the LOGS_FOLDER is created or not, if not it will create
+
+USERID=$(id-u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R please run this script with root privilages $N"
+        exit 1
+
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is ... $R FAILED $N" | tee -a $LOGS_FOLDER
+        exit 1
+    else
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOGS_FOLDER
+    fi
+
+}
+
+echo "script started executing at: $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf install mysql-server -y &>>LOG_FILE
+VALIDATE $? "INSTALLING MySQL Server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabled MySQL Server"
+
+systemctl start mysqld &>>LOG_FILE
+VALIDATE $? "Started MySQL Server"
+
+mysql -h mysql.naveenkadari.com -u root -pExpenseApp@1 -e 'show databases;' &>>LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo 'MySQl root password is not setup, setting now" &>>LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseAPP@1
+    VALIDATE $? "Setting UP root password"
+else
+    echo -e "MySQL root password is already setup..$Y SKIPPING $N"
+fi
