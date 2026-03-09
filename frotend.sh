0a7c47e73934828f7c196965ab74f573500b662d
Naveen Kadari <nkadari1396@gmail.com>
2026-03-09 23:37:22 +0800

    Installing Git

--- frotend.sh
+++ frotend.sh
@@ -34,7 +34,7 @@ echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf install nginx -y &>>LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
 VALIADATE $? "Installing Nginx"
 
 systemctl enable nginx &>>$LOG_FILE
